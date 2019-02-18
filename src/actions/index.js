ff8cfb2c479567dff53b2e9d112d181ddfcb4b14
Asad Manzoor <asad33manzoor@gmail.com>
2019-02-18 18:09:41 +0500

    Minor typo issue fixed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'e5c5a6e6a5bdbdc58f6c5ba80f251d21';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
